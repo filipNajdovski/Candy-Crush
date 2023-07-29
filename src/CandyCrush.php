cbed50fefddb8a952dbf8f5e76cfd6707e054db0
filipNajdovski <filip-najdovski@live.com>
2023-07-29 19:10:27 +0200

    6 test cases correct out of 10, debugging for the problem

--- src/CandyCrush.php
+++ src/CandyCrush.php
@@ -1,17 +1,47 @@
 <?php
-
 class CandyCrush {
     public function howLong($times, $position) {
         $n = count($times);
-        $left = 0;
-        $right = 0;
-        for ($i = 0; $i < $n; $i++) {
-            if ($i < $position) {
-                $left = max($left, $times[$i] - ($position - $i));
-            } else if ($i > $position) {
-                $right = max($right, $times[$i] - ($i - $position));
+        
+        // Helper function to check if the candy can move to a specific position
+        $canMoveToPosition = function($index) use ($times) {
+            return $index >= 0 && $index < count($times) && $times[$index] > 0;
+        };
+        
+        // Helper function to find the maximum crush time of adjacent positions
+        $findMaxCrushTime = function($index) use ($times, $canMoveToPosition) {
+            $maxCrushTime = 0;
+            if ($canMoveToPosition($index - 1)) {
+                $maxCrushTime = max($maxCrushTime, $times[$index - 1]);
+            }
+            if ($canMoveToPosition($index + 1)) {
+                $maxCrushTime = max($maxCrushTime, $times[$index + 1]);
+            }
+            return $maxCrushTime;
+        };
+        
+        $maxTime = 0;
+        $currentIndex = $position;
+        
+        // Simulate candy movement until it can't move anymore
+        while ($canMoveToPosition($currentIndex)) {
+            $maxCrushTime = $findMaxCrushTime($currentIndex);
+            $maxTime = max($maxTime, $maxCrushTime);
+            
+            // Mark the current position as crushed
+            $times[$currentIndex] = 0;
+            
+            // Decide the next position to move based on the maximum crush time
+            if ($canMoveToPosition($currentIndex - 1) && $times[$currentIndex - 1] === $maxCrushTime) {
+                $currentIndex--;
+            } elseif ($canMoveToPosition($currentIndex + 1) && $times[$currentIndex + 1] === $maxCrushTime) {
+                $currentIndex++;
+            } else {
+                // Candy can't move anymore, break the loop
+                break;
             }
         }
-        return max($left, $right);
+        
+        return $maxTime;
     }
 }
