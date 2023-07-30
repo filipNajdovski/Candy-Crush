7ce1d5405295056705f0321fb56778ba16406ed2
filipNajdovski <filip-najdovski@live.com>
2023-07-30 22:03:51 +0200

    Final commit all test cases are correct

--- src/CandyCrush.php
+++ src/CandyCrush.php
@@ -1,33 +1,69 @@
 <?php
 
 class CandyCrush {
+    const INF = 100000;
+
+    private $times; // Define $times property
+    private $n;     // Define $n property
+    private $dp;    // Define $dp property
+    
     public function howLong($times, $position) {
-        $n = count($times);
-        $maxTime = $times[$position];
-
-        // Helper function to check if a move is valid
-        $isValidMove = function ($pos) use ($n, $times) {
-            return $pos >= 0 && $pos < $n && $times[$pos] > 0;
-        };
-
-        while (true) {
-            $leftPosition = $position - 1;
-            $rightPosition = $position + 1;
-
-            if ($isValidMove($rightPosition) && $times[$rightPosition] > $maxTime) {
-                $maxTime = $times[$rightPosition];
-                $position = $rightPosition;
-            } elseif ($isValidMove($leftPosition) && $times[$leftPosition] > $maxTime) {
-                $maxTime = $times[$leftPosition];
-                $position = $leftPosition;
-            } else {
-                break;
-            }
+        $this->times = $times;
+        $this->n = count($times);
+        $this->dp = array_fill(0, $this->n, array_fill(0, max($times)+1, -1));
+        
+        return $this->findMaxTime($position, 0);
+    }
+    
+    private function findMaxTime($position, $time) {
+        if ($position < 0 || $position >= $this->n || $time >= $this->times[$position]) {
+            return $time;
         }
 
-        return $maxTime;
+        if ($this->dp[$position][$time] != -1) {
+            return $this->dp[$position][$time];
+        }
+        
+        $stay = $this->findMaxTime($position, $time + 1);
+        $left = $this->findMaxTime($position - 1, $time + 1);
+        $right = $this->findMaxTime($position + 1, $time + 1);
+
+        $this->dp[$position][$time] = max($stay, $left, $right);
+        
+        return $this->dp[$position][$time];
     }
 }
+
+
+// class CandyCrush {
+//     public function howLong($times, $position) {
+//         $n = count($times);
+//         $maxTime = $times[$position];
+
+//         // Helper function to check if a move is valid
+//         $isValidMove = function ($pos) use ($n, $times) {
+//             return $pos >= 0 && $pos < $n && $times[$pos] > 0;
+//         };
+
+//         while (true) {
+//             $leftPosition = $position - 1;
+//             $rightPosition = $position + 1;
+
+//             // Move right if position valid and i 
+//             if ($isValidMove($rightPosition) && $times[$rightPosition] > $maxTime) {
+//                 $maxTime = $times[$rightPosition];
+//                 $position = $rightPosition;
+//             } elseif ($isValidMove($leftPosition) && $times[$leftPosition] > $maxTime) {
+//                 $maxTime = $times[$leftPosition];
+//                 $position = $leftPosition;
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         return $maxTime;
+//     }
+// }
 
 
 // class CandyCrush {
