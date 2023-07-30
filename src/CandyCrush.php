cdd87e5e9a92ea6a038b4f265d9f6f234d55cf2a
filipNajdovski <filip-najdovski@live.com>
2023-07-30 14:18:05 +0200

    test case 4 and 5 are incorrect

--- src/CandyCrush.php
+++ src/CandyCrush.php
@@ -2,14 +2,14 @@
 class CandyCrush {
     public function howLong($times, $position) {
         $n = count($times);
-        
+
         // Helper function to check if the candy can move to a specific position
-        $canMoveToPosition = function($index) use ($times) {
-            return $index >= 0 && $index < count($times) && $times[$index] > 0;
+        $canMoveToPosition = function($index) use ($n) {
+            return $index >= 0 && $index < $n;
         };
-        
+
         // Helper function to find the maximum crush time of adjacent positions
-        $findMaxCrushTime = function($index) use ($times, $canMoveToPosition) {
+        $findMaxCrushTime = function($index) use ($times, $canMoveToPosition, $n) {
             $maxCrushTime = 0;
             if ($canMoveToPosition($index - 1)) {
                 $maxCrushTime = max($maxCrushTime, $times[$index - 1]);
@@ -19,18 +19,18 @@ class CandyCrush {
             }
             return $maxCrushTime;
         };
-        
+
         $maxTime = 0;
         $currentIndex = $position;
-        
-        // Simulate candy movement until it can't move anymore
-        while ($canMoveToPosition($currentIndex)) {
+
+        // Simulate candy movement until it can't move anymore or maxCrushTime is 0
+        while ($canMoveToPosition($currentIndex) && $times[$currentIndex] > 0) {
             $maxCrushTime = $findMaxCrushTime($currentIndex);
             $maxTime = max($maxTime, $maxCrushTime);
-            
+
             // Mark the current position as crushed
             $times[$currentIndex] = 0;
-            
+
             // Decide the next position to move based on the maximum crush time
             if ($canMoveToPosition($currentIndex + 1) && $times[$currentIndex + 1] === $maxCrushTime) {
                 $currentIndex++;
@@ -42,7 +42,7 @@ class CandyCrush {
                 break;
             }
         }
-        
+
         return $maxTime;
     }
 }
